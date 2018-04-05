9791a72d53bed88698653f6f103313eae9c5d88e
Imre Kiss <jakab.asd@gmail.com>
2018-04-05 14:31:00 +0200

    Fix syntax and style issues.

--- src/state/workout/interval/index.js
+++ src/state/workout/interval/index.js
@@ -4,10 +4,10 @@ import initialState from './initial.state';
 export const SET_ACTIVE = 'SET_ACTIVE';
 
 // Actions
-export const setActive = active => {
+export const setActive = active => ({
   type: SET_ACTIVE,
   active
-};
+});
 
 // Selectors
 export const getActive = state => state.workout.interval.active;
@@ -16,7 +16,7 @@ export const getActive = state => state.workout.interval.active;
 export default (state = initialState, action = {}) => {
   switch (action.type) {
     case SET_ACTIVE:
-      return {...state, active: action.active}
+      return {...state, active: action.active};
     default:
       return state;
   }
