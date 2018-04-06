98a4eaa52a59450889394a9d8c3a788b87b7c1e4
Imre Kiss <jakab.asd@gmail.com>
2018-04-06 19:20:07 +0200

    Change the interval state in case of valid form submit

--- src/components/workout/interval-form/interval-form.container.js
+++ src/components/workout/interval-form/interval-form.container.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import * as formState from '../../../state/workout/interval-form';
+import { setActive } from '../../../state/workout/interval';
 import IntervalForm from './interval-form.component';
 
 const mapStateToProps = state => ({
@@ -9,7 +10,10 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  handleSubmit: event => event.preventDefault(),
+  handleSubmit: event => {
+    event.preventDefault();
+    dispatch(setActive(true));
+  },
   handleResetClick: () => dispatch(formState.resetForm()),
   handleRepeatChange: value => dispatch(formState.setRepeat(formState.positiveValue(value))),
   handleWorkChange: value => dispatch(formState.setWork(formState.positiveValue(value))),
