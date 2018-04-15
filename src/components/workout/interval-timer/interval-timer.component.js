2048e21259877a1958a6c605a808b5a051dab038
Imre Kiss <jakab.asd@gmail.com>
2018-04-15 09:38:50 +0200

    Add details to the interval-timer component

--- src/components/workout/interval-timer/interval-timer.component.js
+++ src/components/workout/interval-timer/interval-timer.component.js
@@ -7,13 +7,37 @@ export default class IntervalTimer extends React.Component {
     super(props);
   }
 
+  componentWillUnmount() {
+    this.props.handleResetClick();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.round !== nextProps.round && nextProps.round > nextProps.repeat) {
+      this.props.handleStopClick(this.props.intervalId);
+    }
+  }
+
   render() {
-    const { active, handleStartStopClick, handleSettingsClick } = this.props;
+    const {
+      active,
+      percentage,
+      color,
+      repeat,
+      round,
+      mainTime,
+      overallTime,
+      intervalId,
+      handleStartClick,
+      handleStopClick,
+      handleResetClick,
+      handleSettingsClick
+    } = this.props;
+
     const digitStyle = {
       fontSize: '220px'
     };
     const progressBarStyle = {
-      width: '50%'
+      width: `${percentage}%`
     };
 
     return (
@@ -23,17 +47,21 @@ export default class IntervalTimer extends React.Component {
           <div className="col text-center">
             <div className="row">
               <div className="col">
-                <span className="display-1" style={digitStyle}>00:00:00</span>
+                <p className={`display-1 font-weight-bold font-monospace text-${color}`} style={digitStyle}>
+                  {mainTime}
+                </p>
               </div>
             </div>
             <div className="row">
               <div className="col text-right border-right">
                 <p className="h4 mb-0">Overall time</p>
-                <p className="display-3">00:00:00</p>
+                <p className="display-4 font-weight-bold font-monospace">{overallTime}</p>
               </div>
               <div className="col text-left border-left">
                 <p className="h4 mb-0">Rounds</p>
-                <p className="display-3">0/10</p>
+                <p className="display-4 font-weight-bold font-monospace">
+                  {`${round > repeat ? round - 1 : round}/${repeat}`}
+                </p>
               </div>
             </div>
           </div>
@@ -46,7 +74,7 @@ export default class IntervalTimer extends React.Component {
                 className="progress-bar bg-info"
                 role="progressbar"
                 style={progressBarStyle}
-                aria-valuenow="50"
+                aria-valuenow={percentage}
                 aria-valuemin="0"
                 aria-valuemax="100">
               </div>
@@ -57,18 +85,26 @@ export default class IntervalTimer extends React.Component {
         <div className="row justify-content-center mt-4">
           <div className="col-auto">
             {active ? (
-              <div className="btn btn-outline-danger" onClick={handleStartStopClick}>Stop</div>
+              <div className="btn btn-outline-danger" onClick={() => handleStopClick(intervalId)}>
+                Stop
+              </div>
             ) : (
-              <div className="btn btn-outline-success" onClick={handleStartStopClick}>Start</div>
+              <button className="btn btn-outline-success" disabled={round > repeat} onClick={handleStartClick}>
+                Start
+              </button>
             )}
           </div>
 
           <div className="col-auto">
-            <div className="btn btn-outline-danger">Reset</div>
+            <button className="btn btn-outline-danger" disabled={active} onClick={handleResetClick}>
+              Reset
+            </button>
           </div>
 
           <div className="col-auto">
-            <div className="btn btn-outline-secondary" onClick={handleSettingsClick}>Settings</div>
+            <button className="btn btn-outline-secondary" disabled={active} onClick={handleSettingsClick}>
+              Settings
+            </button>
           </div>
         </div>
       </div>
@@ -77,7 +113,16 @@ export default class IntervalTimer extends React.Component {
 }
 
 IntervalTimer.propTyypes = {
+  repeat: PropTypes.number.isRequired,
+  round: PropTypes.number.isRequired,
   active: PropTypes.bool.isRequired,
-  handleStartStopClick: PropTypes.func.isRequired,
+  percentage: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
+  mainTime: PropTypes.string.isRequired,
+  overallTime: PropTypes.string.isRequired,
+  intervalId: PropTypes.number,
+  handleStartClick: PropTypes.func.isRequired,
+  handleStopClick: PropTypes.func.isRequired,
+  handleResetClick: PropTypes.func.isRequired,
   handleSettingsClick: PropTypes.func.isRequired
 };
--- src/components/workout/interval-timer/interval-timer.container.js
+++ src/components/workout/interval-timer/interval-timer.container.js
@@ -3,17 +3,61 @@ import IntervalTimer from './interval-timer.component';
 import * as form from '../../../state/workout/interval-form';
 import * as interval from '../../../state/workout/interval';
 import { toggleVisibility } from '../../../state/workout/interval';
+import * as timer from '../../../state/workout/timer';
+
+const getCountDown = state => {
+  const work = form.getWork(state);
+  const rest = form.getRest(state);
+  const repeat = form.getRepeat(state);
+  const clock = timer.getClock(state);
+  const mod = clock % (work + rest);
+  return clock === ((work + rest) * repeat) ? {
+    value: 0,
+    color: 'dark'
+  } : mod < work ? {
+    value: work - mod,
+    color: 'info'
+  } : {
+    value: rest - (mod - work),
+    color: 'warning'
+  };
+};
+
+const getRound = state => {
+  const work = form.getWork(state);
+  const rest = form.getRest(state);
+  return Math.floor(timer.getClock(state) / (work + rest)) + 1;
+};
+
+const getPercentage = state => (
+  Math.trunc(timer.getClock(state) / ((form.getWork(state) + form.getRest(state)) * form.getRepeat(state)) * 100)
+);
 
 const mapStateToProps = state => ({
-  work: form.getWork(state),
-  rest: form.getRest(state),
   repeat: form.getRepeat(state),
-  active: interval.getActive(state)
+  round: getRound(state),
+  active: interval.getActive(state),
+  percentage: getPercentage(state),
+  color: getCountDown(state).color,
+  mainTime: timer.getTime(getCountDown(state).value),
+  overallTime: timer.getTime(timer.getClock(state)),
+  intervalId: timer.getInterval(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-  handleStartStopClick: () => dispatch(interval.toggleActive()),
-  handleResetClick: () => console.log('reset'),
+  handleStartClick: () => {
+    dispatch(interval.setActive(true));
+    dispatch(timer.startTimer(
+      Date.now(),
+      setInterval(() => dispatch(timer.increaseTimer()), 1000)
+    ));
+  },
+  handleStopClick: intervalId => {
+    dispatch(interval.setActive(false));
+    clearInterval(intervalId);
+    dispatch(timer.stopTimer());
+  },
+  handleResetClick: () => dispatch(timer.resetTimer()),
   handleSettingsClick: () => dispatch(toggleVisibility())
 });
 
