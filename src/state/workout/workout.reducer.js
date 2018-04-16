b2c533c492eb63d3a09149b035d72dc674e0cd77
Imre Kiss <jakab.asd@gmail.com>
2018-04-16 21:44:20 +0200

    Add the countdown state to workout

--- src/state/workout/workout.reducer.js
+++ src/state/workout/workout.reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import interval from './interval';
 import intervalForm from './interval-form';
+import countdown from './countdown';
 import timer from './timer';
 
 export default combineReducers({
   interval,
   intervalForm,
+  countdown,
   timer
 });
