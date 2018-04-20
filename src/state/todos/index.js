e767da60782cdf7302ca8bdd38cc96145a063c65
Imre Kiss <jakab.asd@gmail.com>
2018-04-20 23:11:42 +0200

    Add a basic todos state

---
+++ src/state/todos/index.js
@@ -0,0 +1,15 @@
+import initialState from './initial.state';
+
+// Action types
+
+// Action creators
+
+// Selectors
+
+// Reducers
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
