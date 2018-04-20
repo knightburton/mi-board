bec802887bdfd9f0cab3c48f3d685ef2f2fee473
Imre Kiss <jakab.asd@gmail.com>
2018-04-20 23:38:05 +0200

    Add todos state actions action creators and the reducer

--- src/state/todos/index.js
+++ src/state/todos/index.js
@@ -1,14 +1,73 @@
 import initialState from './initial.state';
 
 // Action types
+export const ADD_TODO = 'ADD_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const COMPLETE_ALL_TODOS = 'COMPLETE_ALL_TODOS';
+export const REMOVE_COMPLETED_TODOS = 'REMOVE_COMPLETED_TODOS';
 
 // Action creators
+export const addTodo = text => ({
+  type: ADD_TODO,
+  text
+});
+
+export const removeTodo = id => ({
+  type: REMOVE_TODO,
+  id
+});
+
+export const completeTodo = id => ({
+  type: COMPLETE_TODO,
+  id
+});
+
+export const completeAll = () => ({
+  type: COMPLETE_ALL_TODOS
+});
+
+export const removeCompleted = () => ({
+  type: REMOVE_COMPLETED_TODOS
+});
 
 // Selectors
+export const getTodos = state => state.todos.list;
 
 // Reducers
 export default (state = initialState, action = {}) => {
   switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        list: [...state.list, {
+          id: state.list.length,
+          completed: false,
+          text: action.text
+        }]
+      };
+    case REMOVE_TODO:
+      return {
+        ...state,
+        list: state.list.filter(todo => todo.id !== action.id)
+      };
+    case COMPLETE_TODO:
+      return {
+        ...state,
+        list: state.list.map(todo => todo.id === action.id ? {...todo, completed: !todo.completed} : todo)
+      };
+    case COMPLETE_ALL_TODOS: {
+      const alreadyCompleted = state.every(({completed}) => completed);
+      return {
+        ...state,
+        list: state.list.map(todo => ({...todo, completed: !alreadyCompleted}))
+      };
+    }
+    case REMOVE_COMPLETED_TODOS:
+      return {
+        ...state,
+        list: state.list.filter(todo => todo.completed === false)
+      };
     default:
       return state;
   }
