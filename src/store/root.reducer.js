25b8958cc696ebc829fb263c657c6b1657780aac
Imre Kiss <jakab.asd@gmail.com>
2019-05-03 08:19:18 +0200

    Add time store first version actions, action creatorts and reducer

--- src/store/root.reducer.js
+++ src/store/root.reducer.js
@@ -4,9 +4,11 @@ import { firestoreReducer as firestore } from 'redux-firestore';
 import { firebaseReducer as firebase } from 'react-redux-firebase';
 
 import { reducer as user } from './user';
+import { reducer as time } from './time';
 
 export default combineReducers({
   user,
+  time,
   firestore,
   firebase
 });
---
+++ src/store/time/index.js
@@ -0,0 +1,132 @@
+import { createAction, handleActions } from 'redux-actions';
+
+/**
+ * CONSTANTS
+ */
+export const MIN_CLOCK_VALUE = 0;
+export const MAX_CLOCK_VALUE = 86400;
+
+/**
+ * INITIAL STATE
+ */
+
+export const intialState = {
+  offset: null,
+  clock: 0,
+  intervalID: null,
+  interval: {
+    work: 0,
+    rest: 0,
+    repeat: 0
+  },
+  countdown: {
+    time: 0
+  },
+  stopwatch: {
+    laps: []
+  }
+};
+
+/**
+ * ACTION TYPES
+ */
+
+export const START_TIMER = 'START_TIMER';
+export const STOP_TIMER = 'STOP_TIMER';
+export const RESET_TIMER = 'RESET_TIMER';
+export const INCREASE_TIMER = 'INCREASE_TIMER';
+export const DECREASE_TIMER = 'DECREASE_TIMER';
+
+export const SET_INTERVAL_PARAMS = 'SET_INTERVAL_PARAMS';
+export const RESET_INTERVAL_PARAMS = 'RESET_INTERVAL_PARAMS';
+
+export const SET_COUNTDOWN_TIME = 'SET_COUNTDOWN_TIME';
+export const RESET_COUNTDOWN_TIME = 'RESET_COUNTDOWN_TIME';
+
+export const ADD_STOPWATCH_LAP = 'ADD_STOPWATCH_LAP';
+export const CLEAR_STOPWATCH_LAPS = 'CLEAR_STOPWATCH_LAPS';
+
+/**
+ * ACTION CREATORS
+ */
+
+export const startTimer = createAction(
+  START_TIMER,
+  (offset, intervalID) => ({ offset, intervalID })
+);
+export const stopTimer = createAction(
+  STOP_TIMER
+);
+export const resetTimer = createAction(
+  RESET_TIMER
+);
+export const increaseTimer = createAction(
+  INCREASE_TIMER
+);
+export const decreaseTimer = createAction(
+  DECREASE_TIMER
+);
+
+export const setIntervalParams = createAction(
+  SET_INTERVAL_PARAMS,
+  (work, rest, repeat) => ({ work, rest, repeat })
+);
+export const resetIntervalParams = createAction(
+  RESET_INTERVAL_PARAMS
+);
+
+export const setCountdownTime = createAction(
+  SET_COUNTDOWN_TIME,
+  time => time
+);
+export const resetCountdownTime = createAction(
+  RESET_COUNTDOWN_TIME
+);
+
+export const addStopwatchLap = createAction(
+  ADD_STOPWATCH_LAP,
+  lap => lap
+);
+export const clearStopwatchLaps = createAction(
+  CLEAR_STOPWATCH_LAPS
+);
+
+/**
+ * SELECTORS
+ */
+
+export const getOffset = state => state.time.offset;
+export const getClock = state => state.time.clock;
+export const getIntervalID = state => state.time.intervalID;
+
+export const getIntervalParams = state => state.time.interval;
+export const getIntervalWork = state => state.time.interval.work;
+export const getIntervalRest = state => state.time.interval.rest;
+export const getIntervalRepeat = state => state.time.interval.repeat;
+
+export const getCountdownTime = state => state.time.countdown.time;
+
+export const getStopwatchLaps = state => state.time.stopwatch.laps;
+
+/**
+ * REDUCER
+ */
+export const reducer = handleActions(
+  {
+    [startTimer]: (state, { payload: { offset, intervalID } }) => ({ ...state, offset, intervalID }),
+    [stopTimer]: state => ({ ...state, intervalID: null }),
+    [resetTimer]: state => ({ ...state, clock: 0 }),
+    [increaseTimer]: state => ({ ...state, clock: state.clock === MAX_CLOCK_VALUE ? state.clock : ++state.clock }),
+    [decreaseTimer]: state => ({ ...state, clock: state.clock === MIN_CLOCK_VALUE ? state.clock : --state.clock }),
+
+    [setIntervalParams]: (state, { payload: { work, rest, repeat } }) => ({ ...state, interval: { work, rest, repeat } }),
+    [resetIntervalParams]: state => ({ ...state, interval: intialState.interval }),
+
+    [setCountdownTime]: (state, { payload: time }) => ({ ...state, countdown: { time } }),
+    [resetCountdownTime]: state => ({ ...state, countdown: intialState.countdown }),
+
+    [addStopwatchLap]: (state, { payload: lap }) => ({ ...state, stopwatch: { lap: [ ...state.stopwatch.laps, lap ] } }),
+    [clearStopwatchLaps]: state => ({ ...state, stopwatch: intialState.stopwatch })
+  },
+  intialState
+);
