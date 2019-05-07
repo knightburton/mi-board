4700de832e97db61ec15c3d9cdf285c3aa7c1db3
Imre Kiss <jakab.asd@gmail.com>
2019-05-07 07:55:29 +0200

    Add helpers with basic elapsed time calculations

---
+++ src/helpers/index.js
@@ -0,0 +1,15 @@
+// TIME HELPERS
+
+export const getElapsedHours = value => Math.floor(value / 3600);
+
+export const getElapsedMinutes = value => Math.floor(value % 3600 / 60);
+
+export const getElapsedSeconds = value => Math.floor(value % 3600 % 60);
+
+export const getElapsedTimeString = value => {
+  const h = getElapsedHours(value);
+  const m = getElapsedMinutes(value);
+  const s = getElapsedSeconds(value);
+
+  return `${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`;
+};
