e499f3ea728dfaddc5b392d9d92af2a68d6d280c
Imre Kiss <jakab.asd@gmail.com>
2019-05-21 13:39:02 +0200

    Change the mui theme palette

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactReduxFirebaseProvider from 'react-redux-firebase/lib/ReactReduxFirebaseProvider';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
 import { createFirestoreInstance } from 'redux-firestore';
 import { Router } from 'react-router-dom';
@@ -30,12 +30,12 @@ const rrfProps = {
 ReactDOM.render(
   <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
-      <ThemeProvider theme={theme}>
+      <MuiThemeProvider theme={theme}>
         <CssBaseline />
         <Router history={history}>
           <App />
         </Router>
-      </ThemeProvider>
+      </MuiThemeProvider>
     </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,16 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import deepOrange from '@material-ui/core/colors/deepOrange';
 
-export default createMuiTheme();
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#b5b5b7',
+      main: '#677077',
+      dark: '#252839',
+      contrastText: '#fff'
+    },
+    secondary: deepOrange,
+  },
+});
+
+export default theme;
