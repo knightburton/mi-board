541519ba06e2f44bdb9a8d271d5ee163008623cd
Imre Kiss <jakab.asd@gmail.com>
2019-06-02 23:28:54 +0200

    Update the time constants

--- src/components/time/time.constants.js
+++ src/components/time/time.constants.js
@@ -2,26 +2,14 @@ export const TOOLS = [
   {
     key: 'interval',
     label: 'Interval',
-    to: '/time/interval',
-    text: `
-      A digital circuit that is used to determine the time interval between an initial trigger pulse
-      and subsequent logic states that appear after a predetermined delay.
-    `
+    to: '/time/interval'
   }, {
     key: 'countdown',
     label: 'Countdown',
-    to: '/time/countdown',
-    text: `
-      A countdown timer is a virtual clock on a landing page that counts down from a certain number
-      or date to indicate the beginning or end of an event or offer.
-    `
+    to: '/time/countdown'
   }, {
     key: 'stopwatch',
     label: 'Stopwatch',
-    to: '/time/stopwatch',
-    text: `
-      A clock that can be started and stopped in order to measure the exact time of an event,
-      especially a sports event.
-    `
+    to: '/time/stopwatch'
   }
 ];
