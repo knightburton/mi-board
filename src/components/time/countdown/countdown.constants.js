8ee7b16624f8503bb1979980cd08817448b2c0c2
Imre Kiss <jakab.asd@gmail.com>
2019-06-03 23:34:20 +0200

    Change the form control types in countdown

--- src/components/time/countdown/countdown.constants.js
+++ src/components/time/countdown/countdown.constants.js
@@ -1,24 +1,28 @@
-import { CONTROL_TYPES, VALIDATORS, ERROR_TEXTS } from '../../common/form/form.constants';
+import { CONTROL_TYPES } from '../../common/form/form.constants';
 
 export default [
   {
     key: 'minutes',
-    type: CONTROL_TYPES.NUMBER,
-    defaultValue: '',
     label: 'Countdown minutes',
-    required: true,
-    errorTexts: [ ERROR_TEXTS.NUMBER_BETWEEN(0, 60) ],
-    validators: [ VALIDATORS.NUMBER_BETWEEN(0, 60) ],
+    type: CONTROL_TYPES.SLIDER,
+    defaultValue: 0,
+    min: 0,
+    max: 60,
+    step: 1,
+    indicator: true,
+    buttons: true,
     inline: true
   },
   {
     key: 'seconds',
-    type: CONTROL_TYPES.NUMBER,
-    defaultValue: '',
     label: 'Countdown seconds',
-    required: true,
-    errorTexts: [ ERROR_TEXTS.NUMBER_BETWEEN(0, 59) ],
-    validators: [ VALIDATORS.NUMBER_BETWEEN(0, 59) ],
+    type: CONTROL_TYPES.SLIDER,
+    defaultValue: 0,
+    min: 0,
+    max: 59,
+    step: 1,
+    indicator: true,
+    buttons: true,
     inline: true
   }
 ];
