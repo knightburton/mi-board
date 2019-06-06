84cda475ef2c95e74bc74fbead0d0a3d10d6f9cd
Imre Kiss <jakab.asd@gmail.com>
2019-06-06 15:09:02 +0200

    Add padding to the content wrapper bottom

--- src/components/wrapper/wrapper.component.jsx
+++ src/components/wrapper/wrapper.component.jsx
@@ -5,32 +5,7 @@ import { withStyles } from '@material-ui/core/styles';
 
 import Hidden from '@material-ui/core/Hidden';
 
-import { DRAWER_WIDTH } from '../drawer/drawer.constants';
-
-const styles = theme => ({
-  content: {
-    display: 'flex',
-    flexDirection: 'column',
-    flexGrow: 1
-  },
-  contentFull: {
-    marginLeft: 0
-  },
-  contentCollapse: {
-    marginLeft: DRAWER_WIDTH,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  contentExpand: {
-    marginLeft: theme.spacing(9) + 1,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  }
-});
+import styles from './wrapper.styles';
 
 class Wrapper extends React.PureComponent {
   render() {
