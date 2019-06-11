1cf4a42003f4f0765d35b35012b2328f257380ea
Imre Kiss <jakab.asd@gmail.com>
2019-06-11 21:41:55 +0200

    Improve the app store with global waiting indicator and notifications

--- src/store/app/index.js
+++ src/store/app/index.js
@@ -6,7 +6,9 @@ import { createAction, handleActions } from 'redux-actions';
 
 export const initialState = {
   isDrawerOpened: true,
-  isMobileDrawerOpened: false
+  isMobileDrawerOpened: false,
+  isAppWaiting: false,
+  notifications: []
 };
 
 /**
@@ -15,6 +17,12 @@ export const initialState = {
 
 export const TOGGLE_DRAWER = 'TOGGLE_DRAWER';
 export const TOGGLE_MOBILE_DRAWER = 'TOGGLE_MOBILE_DRAWER';
+
+export const SET_APP_WAITING = 'SET_APP_WAITING';
+
+export const ADD_NOTIFICATION = 'ADD_NOTIFICATION';
+export const REMOVE_NOTIFICATION = 'REMOVE_NOTIFICATION';
+export const REMOVE_ALL_NOTIFICATION = 'REMOVE_ALL_NOTIFICATION';
 
 /**
  * ACTION CREATORS
@@ -27,12 +35,30 @@ export const toggleMobileDrawer = createAction(
   TOGGLE_MOBILE_DRAWER
 );
 
+export const setAppWaiting = createAction(
+  SET_APP_WAITING,
+  waiting => waiting
+);
+
+export const addNotification = createAction(
+  ADD_NOTIFICATION,
+  (message, variant) => ({ message, variant })
+);
+export const removeNotification = createAction(
+  REMOVE_NOTIFICATION,
+  key => key
+);
+export const removeAllNotification = createAction(
+  REMOVE_ALL_NOTIFICATION
+);
+
 /**
  * SELECTORS
  */
 
 export const getIsDrawerOpened = state => state.app.isDrawerOpened;
 export const getIsMobileDrawerOpened = state => state.app.isMobileDrawerOpened;
+export const getIsAppWaiting = state => state.app.isAppWaiting;
 
 /**
  * REDUCER
@@ -41,7 +67,20 @@ export const getIsMobileDrawerOpened = state => state.app.isMobileDrawerOpened;
 export const reducer = handleActions(
   {
     [toggleDrawer]: state => ({ ...state, isDrawerOpened: !state.isDrawerOpened }),
-    [toggleMobileDrawer]: state => ({ ...state, isMobileDrawerOpened: !state.isMobileDrawerOpened })
+    [toggleMobileDrawer]: state => ({ ...state, isMobileDrawerOpened: !state.isMobileDrawerOpened }),
+    [setAppWaiting]: (state, { payload: waiting }) => ({ ...state, isAppWaiting: waiting }),
+    [addNotification]: (state, { payload: { message, variant = 'information' } }) => ({
+      ...state,
+      notifications: [
+        ...state.notifications,
+        { key: new Date().getTime(), message, variant }
+      ]
+    }),
+    [removeNotification]: (state, { payload: key }) => ({
+      ...state,
+      notifications: state.notifications.filter(notification => notification.key !== key)
+    }),
+    [removeAllNotification]: state => ({ ...state, notifications: [] })
   },
   initialState
 );
