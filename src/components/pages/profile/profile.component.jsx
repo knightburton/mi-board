fdc5903899fca5040f276250cf538c509ea08f8a
Imre Kiss <jakab.asd@gmail.com>
2019-06-18 13:09:53 +0200

    Add action menu to the profile page first section

--- src/components/pages/profile/profile.component.jsx
+++ src/components/pages/profile/profile.component.jsx
@@ -4,9 +4,16 @@ import PropTypes from 'prop-types';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/DeleteOutline';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SendIcon from '@material-ui/icons/SendOutlined';
+import PasswordIcon from '@material-ui/icons/VpnKeyOutlined';
 
 import Section from '../../commons/section/section.component';
 import Form from '../../commons/form/form.component';
@@ -30,17 +37,72 @@ export default class Profile extends React.PureComponent {
     profileData: {}
   };
 
+  state = {
+    actionsMenu: null
+  };
+
+  handleActionMenuOpen = target => this.setState({ actionsMenu: target });
+
+  handleActionMenuClose = () => this.setState({ actionsMenu: null });
+
   render() {
     const {
       profileData,
       updataAuthAndProfile,
       updataEmail
     } = this.props;
+    const { actionsMenu } = this.state;
 
     return (
       <Container maxWidth="md">
 
         <Section title="Profile">
+          <Grid spacing={0} justify="flex-end" alignItems="flex-start" container>
+            <Grid xs="auto" item>
+              <IconButton
+                aria-label="Actions"
+                aria-controls="actions-menu"
+                aria-haspopup="true"
+                onClick={e => this.handleActionMenuOpen(e.currentTarget)}
+              >
+                <MoreVertIcon />
+              </IconButton>
+              <Menu
+                id="actions-menu"
+                anchorEl={actionsMenu}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right'
+                }}
+                keepMounted
+                open={Boolean(actionsMenu)}
+                onClose={() => this.handleActionMenuClose()}
+              >
+                <MenuItem onClick={() => this.handleActionMenuClose()}>
+                  <ListItemIcon>
+                    <SendIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Send Email verification" />
+                </MenuItem>
+                <MenuItem onClick={() => this.handleActionMenuClose()}>
+                  <ListItemIcon>
+                    <PasswordIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Send Password Reset Email" />
+                </MenuItem>
+                <MenuItem onClick={() => this.handleActionMenuClose()}>
+                  <ListItemIcon>
+                    <DeleteIcon color="error" />
+                  </ListItemIcon>
+                  <ListItemText primary="Delete Profile" primaryTypographyProps={{ color: 'error' }} />
+                </MenuItem>
+              </Menu>
+            </Grid>
+          </Grid>
           <Grid spacing={4} container>
             <Grid xs={12} sm={4} justify="center" alignItems="center" direction="column" item container>
               <Grid xs item>
