4e127f8694a65ae28e5336c9ddcaf6a8a9fba890
Imre Kiss <jakab.asd@gmail.com>
2019-07-08 22:26:04 +0200

    Fix the time page related form buttons

--- src/components/commons/form/buttons/buttons.styles.js
+++ src/components/commons/form/buttons/buttons.styles.js
@@ -9,7 +9,10 @@ export default theme => ({
     textAlign: 'right'
   },
   buttonWrapper: {
-    margin: theme.spacing(1, 1.5, 0, 1.5)
+    margin: theme.spacing(1, 1.5, 0, 1.5),
+    '& button:nth-child(2)': {
+      marginLeft: theme.spacing(2)
+    }
   },
   floated: {
     position: 'absolute',
