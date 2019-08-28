96359ffd13388f1ab793784f411a79c3caab2aa6
Imre Kiss <jakab.asd@gmail.com>
2019-08-28 14:32:04 +0200

    Separate the stopwatch laps list component

---
+++ src/components/pages/time/stopwatch/laps-list/laps-list.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+import Typography from '@material-ui/core/Typography';
+
+import useStyles from '../stopwatch.styles';
+import { getFormattedSeconds } from '../../../../../helpers';
+
+const LapsList = ({ laps, noOfVisibleLaps }) => {
+  const classes = useStyles();
+
+  return [...laps].reverse().splice(0, noOfVisibleLaps).map((lap, index) => (
+    <Typography key={Math.random() + lap} variant="body1" align="center">
+      <Typography variant="body2" className={clsx(classes.lapText, classes.lapIndex)}>
+        {`${laps.length - index}.`}
+      </Typography>
+      <Typography variant="body2" className={classes.lapText}>
+        {getFormattedSeconds(lap)}
+      </Typography>
+    </Typography>
+  ));
+};
+
+LapsList.proptypes = {
+  laps: PropTypes.arrayOf(PropTypes.number).isRequired,
+  noOfVisibleLaps: PropTypes.number.isRequired
+};
+
+export default LapsList;
