fae8e0128918819922b519ced6e7e184bd64c16a
Imre Kiss <jakab.asd@gmail.com>
2019-08-28 12:22:44 +0200

    Refactor the time helper functions

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,19 +1,6 @@
 import moment from 'moment';
 
 // TIME HELPERS
-
-export const getElapsedHours = value => Math.floor(value / 3600);
-
-export const getElapsedMinutes = value => Math.floor((value % 3600) / 60);
-
-export const getElapsedSeconds = value => Math.floor(value % 3600 % 60);
-
-export const getElapsedTimeString = value => {
-  const h = getElapsedHours(value);
-  const m = getElapsedMinutes(value);
-  const s = getElapsedSeconds(value);
-
-  return `${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}:${s < 10 ? `0${s}` : s}`;
-};
-
+export const getTimestamp = () => moment().valueOf();
+export const getFormattedSeconds = seconds => moment().hour(0).minute(0).second(seconds).format('HH:mm:ss');
 export const getFormattedTimestamp = timestamp => moment(+timestamp).format('YYYY-MM-DD HH:mm');
